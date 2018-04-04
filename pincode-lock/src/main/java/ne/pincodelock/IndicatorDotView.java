b1a3385157f3231930396e00d30e9757ca8a5e99
nour-elghareeb <nour.algharip@gmail.com>
2018-04-04 10:42:50 +0200

    some code optimization

--- pincode-lock/src/main/java/ne/pincodelock/IndicatorDotView.java
+++ pincode-lock/src/main/java/ne/pincodelock/IndicatorDotView.java
@@ -2,6 +2,7 @@ package ne.pincodelock;
 
 import android.content.Context;
 import android.os.Build;
+import android.support.annotation.AnyThread;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
@@ -9,7 +10,6 @@ import android.view.LayoutInflater;
 import android.widget.TextView;
 
 /**
- *
  * LinearLayout wrapper for Dot Recycler
  */
 
@@ -24,6 +24,7 @@ public class IndicatorDotView extends ConstraintLayout {
     public IndicatorDotView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init();
+
     }
 
     public IndicatorDotView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -34,7 +35,9 @@ public class IndicatorDotView extends ConstraintLayout {
 
     private void init() {
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.indicator_container, this);
+        inflater.inflate(R.layout.layout_container__indicator_dots_view, this);
+        setTag(getClass().getSimpleName());
+
     }
 
     @Override
@@ -50,13 +53,14 @@ public class IndicatorDotView extends ConstraintLayout {
         setPadding(20, 20, 20, 20);
         setMinimumHeight(130);
         setMinHeight(130);
-        //setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL);
     }
 
+    @AnyThread
     DotRecyclerView getRecycler() {
         return recycler;
     }
 
+    @AnyThread
     public TextView getDotMsgView() {
         return dotMsgView;
     }
